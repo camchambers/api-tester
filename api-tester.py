25ce8cfc110faebfabfc1386748cdd4659b6c39f
Cam Chambers <cam.chambers@uwaterloo.ca>
2020-03-26 16:03:35 -0400

    Completed a working proof of concept

--- api-tester.py
+++ api-tester.py
@@ -1,22 +1,37 @@
 import requests
+import json
 
-host = "https://<hostname>/"
+def main():
+    host = "http://localhost:54321"
 
-headers = {
-    'content-type': 'application/json',
-    'Accept-Charset': 'UTF-8',
-    'x-api-key': "<api key>"
-}
+    headers = {
+        'content-type': 'application/json',
+        'Accept-Charset': 'UTF-8',
+        'x-api-key': "<api key here>"
+    }
 
-endpoints = {
-    {'method': 'get', 'path': 'constituents/profile/000000'},
-    {'method': 'get', 'path': 'constituents/profile/100000'},
-    {'method': 'get', 'path': 'constituents/profile/abcdef'}
-}
+    endpoints = [
+        {'method': 'get', 'path': 'constituents/649040'},
+        {'method': 'get', 'path': 'constituents/profile/649040'},
+        {'method': 'get', 'path': 'constituents/profile/000000'},
+        {'method': 'get', 'path': 'constituents/profile/649040'},
+    ]      
+
+    for endpoint in endpoints:
+        test_endpoint(host, endpoint['path'], headers, endpoint['method'])
 
 def test_endpoint(host, endpoint, headers, method="get", data=''):
-    print("Testing endpoint " + endpoint)
-    url = host + endpoint
-    payload = open("request.json")
-    results = requests.get(url, data=payload, headers=headers)
-    print(results.content)
+    print("Testing endpoint \"" + endpoint + "\"")
+    url = host + "/" + endpoint
+    result = requests.get(url, headers=headers)
+
+    if result.status_code != 200:
+        print("Request failed with response {}\n".format(result.status_code)) 
+        return
+
+    print("Result:")
+    parsed = json.loads(result.content)
+    print(json.dumps(parsed, indent=4)+ "\n")
+
+if __name__=="__main__":
+        main()
